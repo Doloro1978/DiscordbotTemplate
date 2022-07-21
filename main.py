b6d70be46a3a589cb8bb082150431ea825c2df35
Doloro1978 <74507512+Doloro1978@users.noreply.github.com>
2022-07-21 15:28:14 +0100

    Added some commands and comments

--- main.py
+++ main.py
@@ -1,17 +1,35 @@
 import discord
-
 class MyClient(discord.Client):
     async def on_ready(self): # <-- on_ready Event
         print('Logged on as {0}!'.format(self.user)) # print statement to show it logged in
 
     async def on_message(self, message): # <-- on_message Event 
-        print('Message from {0.author}: {0.content}'.format(message)) # A little print statement to show that the bot can read the messages 
-        if message.author != 829807333570773083: #Stops the bot replying to itself
-            if message == "Ping": #Checks to see if the message sent is "Ping"
-                await message.channel.send("Pong") # Replys with "Pong"
+        print('Message from {0.author}: {0.content}'.format(message)) # A little print statement to show that the bot can read the messages
+        message_storage = []
+        if message.author.bot == False: # Check if the message sent was from a bot or not (It will not reply to a bot)
+            if message.content == "Ping": #Checks to see if the message sent is "Ping"
+                print("The bot should say Pong") # <-- This is meant for debugging reasons
+                await message.channel.send("Pong") # Sents a message with the content "Pong"
 
-            if message == "Pong": #Checks to see if the message sent is "Pong"
-                await message.channel.send("Ping") # Replys with "Ping"
+            if message.content == "Pong": #Checks to see if the message sent is "Pong"
+                print("The bot should say Pong") # <-- This is meant for debugging reasons
+                await message.channel.send("Ping") # Sents a message with the content "Ping"
+
+            if message.content == "Reply": #Checks to see if the message sent is "Reply"
+                print("The bot should add a reaction onto the message") # <-- This is meant for debugging reasons
+                await message.reply("I have replied") # It replys to that message
+
+            if message.content == "Pin me": #Checks to see if the message sent is "Pin me"
+                print("The bot should pin this message")
+                await message.pin() # pins that message
+                message_storage = message # Stores the message for future use
+            
+            if message.content == "Unpin that": #Checks to see if the message sent is "Unpin that"
+                print("The bot should have unpinned that")
+                await message_storage.unpin() # Unpins the message that was in message_storage
+                del message_storage # Clears message_storage
+
+                
 
             
 
