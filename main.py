c77107ea1eacde58fcb87110145c651846695e38
Doloro1978 <74507512+Doloro1978@users.noreply.github.com>
2022-07-22 14:01:46 +0100

    Started to rewrite the code of the bot

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 import discord
+from discord.ext import commands # Imports the commands addon 
+# bot = commands.Bot(command_prefix='?') # <-- Uncomment this and comment the thing under to remove the (@-s count toward a prefix) and keep prefix only
+bot = commands.Bot(command_prefix=commands.when_mentioned_or('?')) # @-s will count towards a prefix 
+
+@bot.command() # <-- Copied from doc (https://discordpy.readthedocs.io/en/stable/ext/commands/commands.html)
+async def test(ctx, arg):
+    await ctx.send(arg)
+bot.add_command(test) # TBH idk what this is meant to do
+
 class MyClient(discord.Client):
     message_storage = []
     async def on_ready(self): # <-- on_ready Event
@@ -7,6 +16,7 @@ class MyClient(discord.Client):
     async def on_message(self, message): # <-- on_message Event 
         print('Message from {0.author}: {0.content}'.format(message)) # A little print statement to show that the bot can read the messages
         message_storage = []
+        # All commands bellow are not using the prefix and are just checking each message sent for its content
         if message.author.bot == False: # Check if the message sent was from a bot or not (It will not reply to a bot)
             if message.content == "Ping": #Checks to see if the message sent is "Ping"
                 print("The bot should say Pong") # <-- This is meant for debugging reasons
@@ -26,7 +36,5 @@ class MyClient(discord.Client):
                 f = open("message_storage.txt", "w")
                 f.write(str(message.id))
                 f.close
-
-
 client = MyClient() 
 client.run('ODI5ODA3MzMzNTcwNzczMDgz.GVo7ci.CLvC_FWxx2O6_otQSQR6JfbHmxf-_iubfo0LII')
